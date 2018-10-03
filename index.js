96289ca7640d86280b95fd73f6ece3f0bc47f119
aleclarson <alec.stanford.larson@gmail.com>
2018-10-03 15:13:27 -0400

    major refactor
    - Add `beforeEach` and `afterEach` functions
    - Let `then` be called at any time

--- index.js
+++ index.js
@@ -1,38 +1,4 @@
-let tests = [] // tests for the next run
-let config = null // config for the next runs
-let promise = null // runner promise
-
-function psec(name, test) {
-  tests.push([name, test])
-  if (!promise) promise = new Promise(run)
-}
-
-module.exports = psec
-
-// run the given suite for each case (sequentially)
-psec.each = async function(cases, suite) {
-  let i = -1
-  while (++i < cases.length) {
-    suite(cases[i]) // setup tests for this case
-    await promise // wait for the runner to finish
-  }
-}
-
-// run a function when the current run finishes
-psec.then = function(done) {
-  return promise.then(done)
-}
-
-// configure the next runs
-psec.configure = function() {
-  config = {
-    ...psec.defaults,
-    ...arguments[0],
-  }
-}
-
-// default configuration
-psec.defaults = {
+const defaults = {
   delay: 0.005,
   minTime: 1,
   minSamples: 5,
@@ -42,42 +8,118 @@ psec.defaults = {
   onFinish() {},
 }
 
-function run(done) {
-  process.nextTick(async () => {
-    let _tests = tests.slice(0)
-    let _config = config || psec.defaults
-
-    // let new tests be queued
-    tests.length = 0
-
-    // find the longest test name
-    _config.width = _tests.reduce(longest, 0)
-
-    let i = -1,
-      cycles = {}
-    while (++i < _tests.length) {
-      const [name, test] = _tests[i]
-      try {
-        cycles[name] = await measure(name, test, _config)
-      } catch (e) {
-        _config.onError(e)
-      }
+class Benchmark {
+  constructor() {
+    this.tests = []
+    this.promise = new Promise(done => {
+      this.done = done
+    })
+    this.config = Object.create(defaults)
+    this.before = []
+    this.after = []
+  }
+  run() {
+    this.run = () => {}
+    if (queue.push(this) == 1) {
+      flush()
     }
-
-    done(cycles) // all done!
-    _config.onFinish(cycles)
-
-    // start the next run
-    if (tests.length) {
-      promise = new Promise(run)
-    } else {
-      promise = null
-    }
-  })
+  }
 }
 
-async function measure(name, test, opts) {
-  let { delay, minTime, minSamples } = opts
+/** The benchmark context */
+let ctx = new Benchmark()
+
+/** The queue of benchmarks ready to run. The active benchmark is first. */
+let queue = []
+
+/** Register a test case */
+function psec(name, test) {
+  ctx.tests.push({
+    name,
+    test,
+  })
+  ctx.run()
+}
+
+module.exports = psec
+
+/** Run the given function **before** each sample */
+psec.beforeEach = function(fn) {
+  ctx.before.push(fn)
+}
+
+/** Run the given function **after** each sample */
+psec.afterEach = function(fn) {
+  ctx.after.push(fn)
+}
+
+// For each case in the `cases` array, run a new benchmark using the `setup` function.
+// The `cases` array may contain any value useful for configuring a benchmark.
+// Think of your `setup` function as a benchmark factory.
+psec.each = async function(cases, setup) {
+  let parent = ctx
+  for (let i = 0; i < cases.length; i++) {
+    ctx = new Benchmark()
+    setup(cases[i])
+  }
+  ctx = parent
+}
+
+/** Run a function when the benchmark finishes */
+psec.then = function(done, onError) {
+  return ctx.promise.then(done, onError)
+}
+
+/** Configure the benchmark context */
+psec.configure = function(config) {
+  Object.assign(ctx.config, config)
+}
+
+/** The default configuration */
+psec.defaults = defaults
+
+/**
+ * Internal
+ */
+
+/** Flush the benchmark queue */
+function flush() {
+  if (queue.length) {
+    let bench = queue[0]
+    process.nextTick(() => {
+      run(bench).then(() => {
+        queue.shift()
+        flush()
+      }, console.error)
+    })
+  }
+}
+
+/** Run a benchmark */
+async function run(bench) {
+  let { tests, config } = bench
+
+  // Find the longest test name.
+  config.width = tests.reduce(longest, 0)
+
+  let cycles = {}
+  for (let i = 0; i < tests.length; i++) {
+    let { name, test } = tests[i]
+    try {
+      cycles[name] = await measure(name, test, bench)
+    } catch (e) {
+      config.onError(e)
+    }
+  }
+
+  // all done!
+  config.onFinish(cycles)
+  bench.done(cycles)
+}
+
+/** Measure a performance test */
+async function measure(name, test, bench) {
+  let { delay, minTime, minSamples, onCycle, onSample } = bench.config
 
   delay *= 1e3
   minTime *= 1e3
@@ -98,11 +140,13 @@ async function measure(name, test, opts) {
   if (test.length == 0) {
     let start
     while (true) {
+      bench.before.forEach(call)
       start = process.hrtime()
       test()
 
       samples[n++] = clock(start)
-      opts.onSample(samples[n - 1], cycle)
+      onSample(samples[n - 1], cycle)
+      bench.after.forEach(call)
 
       if (minTime <= clock(t) && minSamples <= n) {
         break // all done!
@@ -117,6 +161,7 @@ async function measure(name, test, opts) {
   else {
     let start
     const next = function() {
+      bench.before.forEach(call)
       start = process.hrtime()
       test(done)
     }
@@ -124,7 +169,8 @@ async function measure(name, test, opts) {
     // called by the test function for every sample
     const done = function() {
       samples[n++] = clock(start)
-      opts.onSample(samples[n - 1], cycle)
+      onSample(samples[n - 1], cycle)
+      bench.after.forEach(call)
 
       if (minTime <= clock(t) && minSamples <= n) {
         return cycle.done() // all done!
@@ -148,7 +194,11 @@ async function measure(name, test, opts) {
   cycle.time = time
   cycle.stats = stats
 
-  opts.onCycle(cycle, opts)
+  onCycle(cycle, bench.config)
+}
+
+function call(fn) {
+  return fn()
 }
 
 function clock(start) {
@@ -254,7 +304,7 @@ const analyze = (function() {
 
 // Longest name reducer
 function longest(len, test) {
-  return Math.max(len, test[0].length)
+  return Math.max(len, test.name.length)
 }
 
 // default cycle reporter
